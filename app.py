001098999d8d3af492a44632ce445a873b3f1e7a
Gabin Deroues <82848500+gabinde@users.noreply.github.com>
2023-02-10 09:10:41 +0000

    changing storage to redis

--- app.py
+++ app.py
@@ -1,79 +1,82 @@
 import sys
+import redis
 from flask import Flask, request
 
 app = Flask(__name__)
-
-dictionary_add = dict()
-dictionary_sub = dict()
-dictionary_mul = dict()
-dictionary_div = dict()
+r = redis.Redis(host='localhost', port=6379, db=0)
 
 @app.route('/add/<int:value1>/<int:value2>', methods=['POST'])
 def add(value1, value2):  
     result_add = int(value1) + int(value2)
     if request.method == 'POST':
-        dictionary_add[len(dictionary_add)] = result_add
-        id_add = len(dictionary_add)
-        ret = "The result of the addition is " + str(result_add) + " ; id resultat = " + str(id_add) + "\n" 
+        key = r.dbsize()
+        r.set(key,result_add)
+        ret = "The result of the addition is " + str(result_add) + " ; id resultat = " + str(key) + "\n" 
     return ret
-
+""""
 @app.route('/add/get_my_add/<int:id>', methods=['GET'])
 def get_add(id):
     if request.method == 'GET':
         val = dictionary_add[id - 1]
         ret = "The result of the addition with id " + str(id) + " is : " + str(val) + "\n" 
     return ret
-
+"""
 @app.route('/sub/<int:value1>/<int:value2>', methods=['POST'])
-def sub(value1, value2):
-    
+def sub(value1, value2):   
     result_sub = int(value1) - int(value2)
     if request.method == 'POST':
-        dictionary_sub[len(dictionary_sub)] = result_sub
-        id_sub = len(dictionary_sub)
-        ret = "The result of the addition is " + str(result_sub) + " ; id resultat = " + str(id_sub) + "\n" 
+        key = r.dbsize()
+        r.set(key,result_sub)
+        ret = "The result of the addition is " + str(result_sub) + " ; id resultat = " + str(key) + "\n" 
     return ret
 
+""""
 @app.route('/sub/get_my_sub/<int:id>', methods=['GET'])
 def get_sub(id):
     if request.method == 'GET':
         val = dictionary_sub[id - 1]
         ret = "The result of the addition with id " + str(id) + " is : " + str(val) + "\n" 
     return ret
+"""
 
 @app.route('/mul/<int:value1>/<int:value2>', methods=['POST'])
 def mul(value1, value2):   
     result_mul = int(value1) * int(value2)
     if request.method == 'POST':
-        dictionary_mul[len(dictionary_mul)] = result_mul
-        id_mul = len(dictionary_mul)
-        ret = "The result of the addition is " + str(result_mul) + " ; id resultat = " + str(id_mul) + "\n" 
+        key = r.dbsize()
+        r.set(key,result_mul)
+        ret = "The result of the addition is " + str(result_mul) + " ; id resultat = " + str(key) + "\n" 
     return ret
 
+
+""""
 @app.route('/mul/get_my_mul/<int:id>', methods=['GET'])
 def get_mul(id):
     if request.method == 'GET':
         val = dictionary_mul[id - 1]
         ret = "The result of the addition with id " + str(id) + " is : " + str(val) + "\n" 
     return ret
+"""
+
 
 @app.route('/div/<int:value1>/<int:value2>', methods=['POST'])
 def div(value1, value2):
-    
     result_div = int(value1) / int(value2)
     if request.method == 'POST':
-        dictionary_div[len(dictionary_div)] = result_div
-        id_div = len(dictionary_div)
-        ret = "The result of the addition is " + str(result_div) + " ; id resultat = " + str(id_div) + "\n" 
+        key = r.dbsize()
+        r.set(key,result_div)
+        ret = "The result of the addition is " + str(result_div) + " ; id resultat = " + str(key) + "\n" 
     return ret
 
-@app.route('/div/get_my_div/<int:id>', methods=['GET'])
+
+
+"""@app.route('/div/get_my_div/<int:id>', methods=['GET'])
 def get_div(id):
     if request.method == 'GET':
         val = dictionary_div[id - 1]
         ret = "The result of the addition with id " + str(id) + " is : " + str(val) + "\n" 
     return ret
-
+"""
 
 
 
