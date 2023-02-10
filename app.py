1ad31841c0866a03eb3eed379e6f9852688706be
Gabin Deroues <82848500+gabinde@users.noreply.github.com>
2023-02-10 08:33:01 +0000

    added functions that allow calculation recovery through a calculation id

--- app.py
+++ app.py
@@ -9,13 +9,19 @@ dictionary_mul = dict()
 dictionary_div = dict()
 
 @app.route('/add/<int:value1>/<int:value2>', methods=['POST'])
-def add(value1, value2):
-    
+def add(value1, value2):  
     result_add = int(value1) + int(value2)
     if request.method == 'POST':
         dictionary_add[len(dictionary_add)] = result_add
         id_add = len(dictionary_add)
         ret = "The result of the addition is " + str(result_add) + " ; id resultat = " + str(id_add) + "\n" 
+    return ret
+
+@app.route('/add/get_my_add/<int:id>', methods=['GET'])
+def get_add(id):
+    if request.method == 'GET':
+        val = dictionary_add[id - 1]
+        ret = "The result of the addition with id " + str(id) + " is : " + str(val) + "\n" 
     return ret
 
 @app.route('/sub/<int:value1>/<int:value2>', methods=['POST'])
@@ -28,14 +34,27 @@ def sub(value1, value2):
         ret = "The result of the addition is " + str(result_sub) + " ; id resultat = " + str(id_sub) + "\n" 
     return ret
 
+@app.route('/sub/get_my_sub/<int:id>', methods=['GET'])
+def get_sub(id):
+    if request.method == 'GET':
+        val = dictionary_sub[id - 1]
+        ret = "The result of the addition with id " + str(id) + " is : " + str(val) + "\n" 
+    return ret
+
 @app.route('/mul/<int:value1>/<int:value2>', methods=['POST'])
-def mul(value1, value2):
-    
+def mul(value1, value2):   
     result_mul = int(value1) * int(value2)
     if request.method == 'POST':
         dictionary_mul[len(dictionary_mul)] = result_mul
         id_mul = len(dictionary_mul)
         ret = "The result of the addition is " + str(result_mul) + " ; id resultat = " + str(id_mul) + "\n" 
+    return ret
+
+@app.route('/mul/get_my_mul/<int:id>', methods=['GET'])
+def get_mul(id):
+    if request.method == 'GET':
+        val = dictionary_mul[id - 1]
+        ret = "The result of the addition with id " + str(id) + " is : " + str(val) + "\n" 
     return ret
 
 @app.route('/div/<int:value1>/<int:value2>', methods=['POST'])
@@ -47,6 +66,16 @@ def div(value1, value2):
         id_div = len(dictionary_div)
         ret = "The result of the addition is " + str(result_div) + " ; id resultat = " + str(id_div) + "\n" 
     return ret
+
+@app.route('/div/get_my_div/<int:id>', methods=['GET'])
+def get_div(id):
+    if request.method == 'GET':
+        val = dictionary_div[id - 1]
+        ret = "The result of the addition with id " + str(id) + " is : " + str(val) + "\n" 
+    return ret
+
+
+
 
 
 
