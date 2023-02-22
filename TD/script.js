96af2135a774c7eca17ec7d8147936158c8d0155
Gabin Deroues <82848500+gabinde@users.noreply.github.com>
2023-02-22 17:33:38 +0000

    changement de nom de variable

--- TD/script.js
+++ TD/script.js
@@ -4,25 +4,17 @@ document.getElementById("calculation-form").addEventListener("submit", function(
     const number1 = document.getElementById("number1").value;
     const number2 = document.getElementById("number2").value;
     const operator = document.getElementById("operator").value;
-    firstArgument = '';
-    if(operator == '+'){
-        firstArgument = 'add';
-    }
-    else if(operator == '-'){
-        firstArgument = 'sub';
-    }
-    else if(operator == '×'){
-        firstArgument == 'mul';
-    } 
-    else if(operator == '÷'){
-        firstArgument == 'div';
-    }
+    const option = {
+        method: 'POST' 
+    };
     
-    fetch(`http://localhost:5000/${firstArgument}/${number1}/${number2}`)
+    fetch(`http://127.0.0.1:5000/${operator}/${number1}/${number2}`, option)
       .then(response => response.json())
       .then(data => {
+        console.log(response.json());
         const calculationId = data.id;
         document.getElementById("result").innerHTML = 5;
       });
   });
   
+
