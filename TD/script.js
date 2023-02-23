522367b43222bc2a5cb47d7cd93ace9f57af2304
Gabin Deroues <82848500+gabinde@users.noreply.github.com>
2023-02-23 08:53:51 +0000

    ajout recupération calcul (TD)

--- TD/script.js
+++ TD/script.js
@@ -16,5 +16,17 @@ document.getElementById("calculation-form").addEventListener("submit", function(
         document.getElementById("result").innerHTML = 5;
       });
   });
+
+document.getElementById("get-calcul").addEventListener("submit", function(e) {
+    e.preventDefault();
+    console.log('vla')
+    const id_recup = document.getElementById("recup").value;
+
+    fetch(`http://127.0.0.1:5000/get_my_calcul/${id_recup}`)
+        .then(response => response.json())
+        .then(data => {
+            console.log('bla')
+        })
+});
   
 
