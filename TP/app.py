effc8bc335589d5efa45ddb6e490feb3168dce5d
Gabin Deroues <82848500+gabinde@users.noreply.github.com>
2023-02-23 10:42:30 +0000

    correction erreur

--- TP/app.py
+++ TP/app.py
@@ -5,7 +5,7 @@ from flask import Flask, request
 app = Flask(__name__)
 r = redis.Redis(host='localhost', port=6379, db=0)
 
-@app.route('/flip/<string:flip>', methods=['POST','GET']) #vérifier quel méthode utiliser
+@app.route('/flip/<flip>', methods=['POST','GET']) #vérifier quel méthode utiliser
 def flip(flip):  
     if request.method == 'POST' or request.method == 'GET':
         key = r.dbsize()
