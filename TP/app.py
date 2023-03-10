abaad986c9eee9eda588028a780e8cf810438513
Gabin Deroues <82848500+gabinde@users.noreply.github.com>
2023-03-10 08:23:45 +0000

    ajout d'un fichier pour supprimer erreur docstring, et ajout d'une fonction pour récupérer l'ensemble des tweets

--- TP/app.py
+++ TP/app.py
@@ -1,22 +1,32 @@
 import sys
+from datetime import datetime
 import redis
 from flask import Flask, request
-from datetime import datetime
 
 app = Flask(__name__)
 rTimeStamps = redis.Redis(host='localhost', port=6379, db=0)
 rUsername = redis.Redis(host='localhost', port=6379, db=1)
 
-@app.route('/flipByTime/<author>/<flip>', methods=['POST']) #vérifier quel méthode utiliser
-def flipByTime(flip,author):  
-    if request.method == 'POST':
+@app.route('/flip/<author>/<flip>', methods=['GET','POST'])
+def flip_by_time(flip,author):
+    """enregiste les flips par auteurs et timestamp"""
+    if request.method == 'POST' or request.method == 'GET':
         key = str(datetime.now())
-        rUsername.lpush( key, "author: " +  author + "flip: " + flip)
-        timeStamp = rUsername.lrange(key,0,-1)
-        timeStamp += ", " + key
-        rTimeStamps.lpush(author, timeStamp)
-    return 0
-    
+        rUsername.lpush( key, "author: " +  author + " flip: " + flip)
+        timestamp = rUsername.lrange(key,0,-1)
+        timestamp +=  key
+        rTimeStamps.lpush(author, str(timestamp))
+    return 'hello \n'
+
+@app.route('/getAllFlip', methods=['GET','POST'])
+def get_flip():
+    """récupère l'ensemble des flips"""
+    flips = []
+    for key in rUsername.scan_iter("*"):
+        flip = rUsername.lrange(key,0,-1)
+        flips.append(str(flip))
+    return flips
+
 
 if __name__ == '__main__':
     if len(sys.argv) > 1:
