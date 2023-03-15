6c294de0e3922eb66a64affdb28400bb3607fe6e
Gabin Deroues <82848500+gabinde@users.noreply.github.com>
2023-03-15 10:38:12 +0000

    added a function to get all the flip of a user

--- TP/app.py
+++ TP/app.py
@@ -1,31 +1,47 @@
 import sys
+import json
+import re
 from datetime import datetime
 import redis
 from flask import Flask, request
 
 app = Flask(__name__)
-rTimeStamps = redis.Redis(host='localhost', port=6379, db=0)
+rTimestamps = redis.Redis(host='localhost', port=6379, db=0)
 rUsername = redis.Redis(host='localhost', port=6379, db=1)
 
 @app.route('/flip/<author>/<flip>', methods=['GET','POST'])
-def flip_by_time(flip,author):
+def flip_by_time(author,flip):
     """enregiste les flips par auteurs et timestamp"""
     if request.method == 'POST' or request.method == 'GET':
-        key = str(datetime.now())
-        rUsername.lpush( key, "author: " +  author + " flip: " + flip)
-        timestamp = rUsername.lrange(key,0,-1)
-        timestamp +=  key
-        rTimeStamps.lpush(author, str(timestamp))
+        timestamp = str(datetime.now())
+        rUsername.lpush( timestamp, "author: " +  author + " flip: " + flip)
+        rTimestamps.lpush(author, timestamp)
     return 'hello \n'
 
 @app.route('/getAllFlip', methods=['GET','POST'])
 def get_flip():
     """récupère l'ensemble des flips"""
-    flips = []
-    for key in rUsername.scan_iter("*"):
-        flip = rUsername.lrange(key,0,-1)
-        flips.append(str(flip))
+    if request.method == 'POST' or request.method == 'GET':
+        flips = []
+        for key in rUsername.scan_iter("*"):
+            flip = rUsername.lrange(key,0,-1)
+            flips.append(str(flip))
     return flips
+
+@app.route('/getFlipByUser/<author>', methods=['GET','POST'])
+def get_flip_by_user(author):
+    """récupère l'ensemble des flips provennant d'un utilisateur"""
+    if request.method == 'POST' or request.method == 'GET':
+        timestamps = rTimestamps.lrange(author,0,-1) #récupère l'ensemle des timestamps des flips d'une personne
+        flip_by_user = []
+        pattern = r"flip: (\w+)" #regex pour extraire les flips
+        for timestamp in timestamps: 
+            flip = rUsername.lrange(timestamp,0,-1) #récupération du flip
+            flip = [x.decode() for x in flip]
+            match = re.search(pattern, str(flip))# application regex
+            flip = match.group(1) #récupère la partie recherché
+            flip_by_user.append(flip)
+    return json.dumps(flip_by_user) #renvoie le tableau avec les users
 
 
 if __name__ == '__main__':
