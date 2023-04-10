d0686ee59d3938a4766fd75140cf58818c0338be
Morgane <lacombre.morgane@live.fr>
2023-04-10 19:39:20 +0200

    ajout front

--- TP/frontend/script.js
+++ TP/frontend/script.js
@@ -8,21 +8,20 @@ document.addEventListener("DOMContentLoaded", function(event) {
     xhr.onload = function() {
       if (xhr.status === 200) {
         let donnees = JSON.parse(xhr.responseText);
-        console.log(donnees);
-        for (let i = 0; i < donnees.length; i++) {
-          let flip = donnees[i].flip;
-          let author = donnees[i].author;
-          let flipDiv = document.createElement("div");
-          flipDiv.classList.add("flip");
-          let flipPseudo = document.createElement("p");
-          flipPseudo.classList.add("flip-pseudo");
-          flipPseudo.textContent = "auteur : " + author;
-          let flipText = document.createElement("p");
-          flipText.classList.add("flip-text");
-          flipText.textContent = flip;
-          flipDiv.appendChild(flipPseudo);
-          flipDiv.appendChild(flipText);
-          document.getElementById("flip-container").appendChild(flipDiv);
+          for (let i = 0; i < donnees.length; i++) {
+            let flip = donnees[i].flip;
+            let author = donnees[i].author;
+            let flipDiv = document.createElement("div");
+            flipDiv.classList.add("flip");
+            let flipPseudo = document.createElement("p");
+            flipPseudo.classList.add("flip-pseudo");
+            flipPseudo.textContent = "auteur : " + author;
+            let flipText = document.createElement("p");
+            flipText.classList.add("flip-text");
+            flipText.textContent = flip;
+            flipDiv.appendChild(flipPseudo);
+            flipDiv.appendChild(flipText);
+            document.getElementById("flip-container").appendChild(flipDiv);
         }
       }
     };
@@ -38,12 +37,12 @@ document.addEventListener("DOMContentLoaded", function(event) {
     xhr2.open('GET', `http://127.0.0.1:5000/getAllSubject`, true);
     xhr2.onload = function() {
       if (xhr2.status === 200) {
-        let donnees = JSON.parse(xhr.responseText);
-        for (let i = 0; i < donnees.length; i++) {
-          let subject = donnees[i].subject;
+        let donneees = JSON.parse(xhr2.responseText);
+        for (let i = 0; i < donneees.length; i++) {
+          let subject = donneees[i];
           let subjectDiv = document.createElement("div");
           subjectDiv.classList.add("subject");
-          let subjectText = document.createElement("p");
+          let subjectText = document.createElement("button");
           subjectText.classList.add("subject-text");
           subjectText.textContent = subject;
           subjectDiv.appendChild(subjectText);
@@ -59,7 +58,7 @@ document.addEventListener("DOMContentLoaded", function(event) {
 
 
   // Récupérer le bouton "Tweet" et le formulaire
-  var btnTweet = document.querySelector("button");
+  var btnTweet = document.getElementById("flip");
   var formTweet = document.getElementById("formulaire");
 
   // Ajouter un écouteur d'événement sur le bouton "Tweet"
@@ -69,6 +68,38 @@ document.addEventListener("DOMContentLoaded", function(event) {
     btnTweet.style.display = "none";
     formTweet.style.display = "block";
 
+  });
+
+  var btnSearch = document.getElementById("username");
+  btnSearch.addEventListener("click", function(event) {
+    let xhr4 = new XMLHttpRequest();
+    let username = document.getElementById("user").value;
+    xhr4.open('GET', `http://127.0.0.1:5000/getFlipByUser/${encodeURIComponent(encodeURI(username))}`, true);
+    xhr4.onload = function() {
+      if (xhr4.status === 200) {
+        let donnees = JSON.parse(xhr4.responseText);
+        document.getElementById("flip-container").innerHTML = "";
+        for (let i = 0; i < donnees.length; i++) {
+          let flip = donnees[i].flip;
+          let author = donnees[i].author;
+          let flipDiv = document.createElement("div");
+          flipDiv.classList.add("flip");
+          let flipPseudo = document.createElement("p");
+          flipPseudo.classList.add("flip-pseudo");
+          flipPseudo.textContent = "auteur : " + author;
+          let flipText = document.createElement("p");
+          flipText.classList.add("flip-text");
+          flipText.textContent = flip;
+          flipDiv.appendChild(flipPseudo);
+          flipDiv.appendChild(flipText);
+          document.getElementById("flip-container").appendChild(flipDiv);
+        }
+      };
+      xhr4.onerror = function() {
+        console.log("Une erreur est survenue lors de la requête");
+      };
+    };
+    xhr4.send();
   });
 
   // Ajouter un écouteur d'événement sur le formulaire
@@ -89,9 +120,7 @@ document.addEventListener("DOMContentLoaded", function(event) {
     xhr3.open('POST', `http://127.0.0.1:5000/flip/${encodeURIComponent(encodeURI(username))}/${encodeURIComponent(encodeURI(tweet))}`, true);
     xhr3.onload = function() {
       if (xhr3.status === 200) {
-        let donnees = JSON.parse(xhr.responseText);
-        console.log(donnees);
-      }
+        let donnees = JSON.parse(xhr.responseText);      }
     };
     xhr3.onerror = function() {
       console.log("Une erreur est survenue lors de la requête");
